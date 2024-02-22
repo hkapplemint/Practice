cc69a90b6fbc16a0cd70e7bc7611dbd18451d490
hkapplemint <laitatwai@gmail.com>
2024-02-22 20:16:59 +0800

    modified:   SortableDragDrop/script.js 	modified:   SortableDragDrop/styles.css

--- SortableDragDrop/script.js
+++ SortableDragDrop/script.js
@@ -17,89 +17,38 @@ tierContainers.forEach( tierContainer => {
 
         const elementBeingDragged = document.querySelector(".participant.dragging")
 
-        const afterElement = getDragAfterElement(tierContainer, event.clientX, event.clientY);
-        
-        if (afterElement == null) {
-            tierContainer.append(elementBeingDragged);
-        } else {
-            tierContainer.insertBefore(elementBeingDragged, afterElement);
-        }
+        const indexOfTargetElement = getIndexToInsert(tierContainer, event.clientX, event.clientY);
 
+        if (tierContainer.children[indexOfTargetElement]) {
+            const afterElement = tierContainer.children[indexOfTargetElement];
+            tierContainer.insertBefore(elementBeingDragged, afterElement);
+        } else {
+            tierContainer.append(elementBeingDragged);
+        }
     })
 })
 
-function getDragAfterElement(container, x, y) {
-    const elementsInThisContainerNotBeingDragged = [...container.querySelectorAll(".participant:not(.dragging)")]
+function getIndexToInsert(container, clientX, clientY) {
+    const elementsInThisContainer = [...container.querySelectorAll(".participant")]
 
-    const elementToReturn = elementsInThisContainerNotBeingDragged.reduce((closest, child) => {
-        const box = child.getBoundingClientRect();
-        const offsetX = x - box.x - box.width/2;
-        const offsetY = y - box.y - box.height;
+    const propertiesOfElements = elementsInThisContainer.map(element => {
+        const boxOfThisElement = element.getBoundingClientRect();
+        return {x: boxOfThisElement.x, y: boxOfThisElement.y, height: boxOfThisElement.height, width: boxOfThisElement.width}
+    })
 
-        if (offsetX < 0 && offsetX > closest.offsetX && offsetY < 0 && offsetY > closest.offsetY) {
-            return {offsetX: offsetX, offsetY: offsetY, element: child}
-        } else {
-            return closest
+    let indexOfTargetElement = 999;
+
+    for (const property of propertiesOfElements) {
+        if (clientX - property.x - property.width/2 < 0 && clientY - property.y - property.height/1.2 < 0) {
+            indexOfTargetElement = propertiesOfElements.indexOf(property);
+            break
+        } else if (clientX - property.x - property.width/2 > 0 && clientY - property.y < 0) {
+            indexOfTargetElement = propertiesOfElements.indexOf(property);
+            break
         }
+    }
 
-    }, {offsetX: Number.NEGATIVE_INFINITY, offsetY: Number.NEGATIVE_INFINITY}).element;
+    console.log(indexOfTargetElement);
 
-    return elementToReturn;
-
-    //return a single element that is the closest to the element being dragged
+    return indexOfTargetElement;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// const containers = document.querySelectorAll(".container")
-// const draggables = document.querySelectorAll(".draggable")
-
-// draggables.forEach(draggable => {
-//     draggable.addEventListener("dragstart", () => {
-//         draggable.classList.add("dragging");
-//     })
-
-//     draggable.addEventListener("dragend", () => {
-//         draggable.classList.remove("dragging");
-//     })
-// })
-
-// containers.forEach(container => {
-//     container.addEventListener("dragover", event => {
-//         event.preventDefault();
-//         const afterElement = getDragAfterElement(container, event.clientY)
-//         const dragging = document.querySelector(".dragging");
-        
-
-//         if (afterElement == null) {
-//             container.append(dragging);
-//         } else {
-//             container.insertBefore(dragging, afterElement);
-//         }
-
-//     })
-// })
-
-// function getDragAfterElement(container, y) {
-//     const draggableElements = [...container.querySelectorAll(".draggable:not(.dragging)")]
-
-//     return draggableElements.reduce((closest, child) => {
-//         const box = child.getBoundingClientRect();
-//         const offset = y - box.top - box.height/2;
-//         if(offset < 0 && offset > closest.offset) {
-//             return {offset: offset, element: child};
-//         } else {
-//             return closest
-//         }
-
-//     }, {offset: Number.NEGATIVE_INFINITY}).element
-// }
