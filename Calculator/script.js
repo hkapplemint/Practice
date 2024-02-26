43c05c0749addde1b98d006d1690f0c489dbe59f
hkapplemint <laitatwai@gmail.com>
2024-02-26 12:23:28 +0800

    modified:   Calculator/index.html 	modified:   Calculator/script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -5,9 +5,16 @@ const deleteBtn = document.getElementById("delete-btn");
 const showMoreBtn = document.getElementById("show-more-btn");
 const showMoreRelateds = document.querySelectorAll(".show-more-related");
 const equalBtn = document.getElementById("equal-btn");
+const dotBtn = document.getElementById("dot-btn");
+const operators = document.querySelectorAll(".operator");
 
 digitBtns.forEach(digitBtn => {
     digitBtn.addEventListener("click", (e) => {
+        display.value += e.currentTarget.textContent;
+    })
+})
+operators.forEach(operatorBtn => {
+    operatorBtn.addEventListener("click", (e) => {
         display.value += e.currentTarget.textContent;
     })
 })
@@ -28,20 +35,102 @@ showMoreBtn.addEventListener("click", () => {
     })
 })
 
-const str = "(12.66/-23)";
-const regexHighOrderInfix = /(-?\d*\.?\d*)(?:[*\/])(-?\d*\.?\d*)/g;
-console.log(regexHighOrderInfix.exec(str)[1]);
 
 const regexNumbers = /[\d]+[.]?[\d]*/g;
-const regexParen = /\(.*\)/g;  //return "(12.34*56.7)"
+const regexParen = /\([^()]+\)/g;
+const regexExtractFormulaHighInfix = /(-?\d+\.?\d*)([x÷])(-?\d+\.?\d*)/;
+const regexExtractFormulaLowInfix = /(-?\d+\.?\d*)([\+\-])(-?\d+\.?\d*)/;
 
-function calculate() {
-    const inputString = display.value;
-    //2340.32/-3.2
+// const str = "(12.66/-23)";
+// console.log(regexHighOrderInfix.exec(str)[1]);
+
+function toFixedAndParseFloat(string) {
+    return parseFloat(parseFloat(string).toFixed(10))
+}
+
+function switchFourOperators(firstNumber, infix, secondNumber) {
+    let result;
+    switch(infix) {
+        case "x":
+            result = toFixedAndParseFloat(firstNumber * secondNumber)
+            break
+        case "÷":
+            result = toFixedAndParseFloat(firstNumber / secondNumber)
+            break
+        case "+":
+            result = toFixedAndParseFloat(firstNumber + secondNumber)
+            break
+        case "-":
+            result = toFixedAndParseFloat(firstNumber - secondNumber)
+            break
+        default:
+            result = NaN;
+            break
+        }
+    return result;
+}
+
+function calculate(inputString) {
+    //const inputString = display.value;
+    //e.g. 230+(12.66*-23)-(32.1-34.1)
     const parenMatches = inputString.match(regexParen);
-    console.log(parenMatches);
+    //parenMatches = ["(12.66*-23)", "(32.1-34.1)"]
+    if(parenMatches) {
+        let leftMostParen = parenMatches.shift();
+        let formula = regexExtractFormulaHighInfix.exec(leftMostParen);
+        if (formula) {
+            let firstNumber = toFixedAndParseFloat(formula[1]);
+            let infix = formula[2];
+            let secondNumber = toFixedAndParseFloat(formula[3]);
+    
+            let result = switchFourOperators(firstNumber, infix, secondNumber)
+    
+            inputString = inputString.replace(leftMostParen, result);
+            calculate(inputString);
+        } else {
+            formula = regexExtractFormulaLowInfix.exec(leftMostParen);
+            firstNumber = toFixedAndParseFloat(formula[1]);
+            infix = formula[2];
+            secondNumber = toFixedAndParseFloat(formula[3]);
+    
+            result = switchFourOperators(firstNumber, infix, secondNumber)
+    
+            inputString = inputString.replace(leftMostParen, result);
+            calculate(inputString);
+        }
+
+    } else {
+        let leftMostFormula = inputString.match(regexExtractFormulaHighInfix);
+        if(leftMostFormula){
+            formula = regexExtractFormulaHighInfix.exec(leftMostFormula);
+            firstNumber = toFixedAndParseFloat(formula[1]);
+            infix = formula[2];
+            secondNumber = toFixedAndParseFloat(formula[3]);
+    
+            result = switchFourOperators(firstNumber, infix, secondNumber);
+            inputString = inputString.replace(leftMostFormula[0], result);
+    
+            calculate(inputString);
+        } else {
+            leftMostFormula = inputString.match(regexExtractFormulaLowInfix);
+            if(!leftMostFormula) {
+                console.log(inputString);
+                display.value = inputString;
+                return;
+            }
+            formula = regexExtractFormulaLowInfix.exec(leftMostFormula);
+            firstNumber = toFixedAndParseFloat(formula[1]);
+            infix = formula[2];
+            secondNumber = toFixedAndParseFloat(formula[3]);
+    
+            result = switchFourOperators(firstNumber, infix, secondNumber);
+            inputString = inputString.replace(leftMostFormula[0], result);
+    
+            calculate(inputString);
+        }
+    }
 }
 
 equalBtn.addEventListener("click", () => {
-    calculate();
+    calculate(display.value);
 })
