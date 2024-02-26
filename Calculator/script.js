b70dbfa602b7b66992e30e48edd84e4204075325
hkapplemint <laitatwai@gmail.com>
2024-02-26 09:59:07 +0800

    modified:   Calculator/index.html 	modified:   Calculator/script.js 	modified:   Calculator/styles.css

--- Calculator/script.js
+++ Calculator/script.js
@@ -2,6 +2,9 @@ const digitBtns = document.querySelectorAll(".digit");
 const display = document.querySelector(".display");
 const allClearBtn = document.getElementById("AC-btn");
 const deleteBtn = document.getElementById("delete-btn");
+const showMoreBtn = document.getElementById("show-more-btn");
+const showMoreRelateds = document.querySelectorAll(".show-more-related");
+const equalBtn = document.getElementById("equal-btn");
 
 digitBtns.forEach(digitBtn => {
     digitBtn.addEventListener("click", (e) => {
@@ -19,9 +22,26 @@ deleteBtn.addEventListener("click", () => {
     }
 })
 
+showMoreBtn.addEventListener("click", () => {
+    showMoreRelateds.forEach(element => {
+        element.classList.toggle("hidden");
+    })
+})
 
-function deleteBtnFx() {
-    if(display.value.length > 0) {
-        display.value = display.value.slice(0, -1);
-    }
+const str = "(12.66/-23)";
+const regexHighOrderInfix = /(-?\d*\.?\d*)(?:[*\/])(-?\d*\.?\d*)/g;
+console.log(regexHighOrderInfix.exec(str)[1]);
+
+const regexNumbers = /[\d]+[.]?[\d]*/g;
+const regexParen = /\(.*\)/g;  //return "(12.34*56.7)"
+
+function calculate() {
+    const inputString = display.value;
+    //2340.32/-3.2
+    const parenMatches = inputString.match(regexParen);
+    console.log(parenMatches);
 }
+
+equalBtn.addEventListener("click", () => {
+    calculate();
+})
