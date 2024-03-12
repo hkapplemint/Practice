5408e1911567d72184db94f315ae48c378bfca2b
hkapplemint <laitatwai@gmail.com>
2024-03-12 13:27:27 +0800

    basic game logic completed. need to add game over and auto movement

--- Snake/snake.js
+++ Snake/snake.js
@@ -7,101 +7,178 @@ const widthRatio = parseInt(gameBoardBox.width / blockBox.width);
 const heightRatio = parseInt(gameBoardBox.height / blockBox.height);
 
 document.addEventListener("keydown", e => {
-  const snakeHead = document.querySelector(".snake-head");
-  const snakeBodies = document.querySelectorAll(".snake-body");
+    const snakeHead = document.querySelector(".snake-head");
+    const snakeBodies = document.querySelectorAll(".snake-body");
 
-  let nextDirection;
-  switch (e.key) {
-    case "ArrowUp":
-      [...snakeBodies].forEach(snakeBody => {
-        nextDirection = calculateNextDirection(snakeBody);
-        move(nextDirection, snakeBody);
-      });
-      move("up", snakeHead);
-      break;
-    case "ArrowDown":
-      [...snakeBodies].forEach(snakeBody => {
-        nextDirection = calculateNextDirection(snakeBody);
-        move(nextDirection, snakeBody);
-      });
-      move("down", snakeHead);
-      break;
-    case "ArrowRight":
-      [...snakeBodies].forEach(snakeBody => {
-        nextDirection = calculateNextDirection(snakeBody);
-        move(nextDirection, snakeBody);
-      });
-      move("right", snakeHead);
-      break;
-    case "ArrowLeft":
-      [...snakeBodies].forEach(snakeBody => {
-        nextDirection = calculateNextDirection(snakeBody);
-        move(nextDirection, snakeBody);
-      });
-      move("left", snakeHead);
-      break;
-  }
+    let targetCol;
+    let targetRow;
+    switch (e.key) {
+        case "ArrowUp":
+            targetCol = snakeHead.dataset.col;
+            targetRow = parseInt(snakeHead.dataset.row) - 1;
+            if (targetIsSnakeBody(targetCol, targetRow) || snakeHead.dataset.row == 0) {
+                console.log("Snake head cannot move up");
+                return;
+            } else if (targetIsFood(targetCol, targetRow)) {
+                eatFood(targetCol, targetRow);
+                snakeGrow();
+            }
+
+            moveBodyPart(snakeBodies);
+            snakeHead.dataset.next = "up";
+            move("up", snakeHead);
+
+            if (foodEaten) generateFood();
+            break;
+        case "ArrowDown":
+            targetCol = snakeHead.dataset.col;
+            targetRow = parseInt(snakeHead.dataset.row) + 1;
+            if (
+                targetIsSnakeBody(targetCol, targetRow) ||
+                snakeHead.dataset.row == heightRatio - 1
+            ) {
+                console.log("Snake head cannot move down");
+                return;
+            } else if (targetIsFood(targetCol, targetRow)) {
+                eatFood(targetCol, targetRow);
+                snakeGrow();
+            }
+
+            moveBodyPart(snakeBodies);
+            snakeHead.dataset.next = "down";
+            move("down", snakeHead);
+
+            if (foodEaten) generateFood();
+            break;
+        case "ArrowRight":
+            targetCol = parseInt(snakeHead.dataset.col) + 1;
+            targetRow = snakeHead.dataset.row;
+            if (
+                targetIsSnakeBody(targetCol, targetRow) ||
+                snakeHead.dataset.col == widthRatio - 1
+            ) {
+                console.log("Snake head cannot move right");
+                return;
+            } else if (targetIsFood(targetCol, targetRow)) {
+                eatFood(targetCol, targetRow);
+                snakeGrow();
+            }
+
+            moveBodyPart(snakeBodies);
+            snakeHead.dataset.next = "right";
+            move("right", snakeHead);
+
+            if (foodEaten) generateFood();
+            break;
+        case "ArrowLeft":
+            targetCol = parseInt(snakeHead.dataset.col) - 1;
+            targetRow = snakeHead.dataset.row;
+            if (targetIsSnakeBody(targetCol, targetRow) || snakeHead.dataset.col == 0) {
+                console.log("Snake head cannot move left");
+                return;
+            } else if (targetIsFood(targetCol, targetRow)) {
+                eatFood(targetCol, targetRow);
+                snakeGrow();
+            }
+
+            moveBodyPart(snakeBodies);
+            snakeHead.dataset.next = "left";
+            move("left", snakeHead);
+
+            if (foodEaten) generateFood();
+            break;
+    }
 });
 
-function calculateNextDirection(element) {
-  const previousBodyPartId = parseInt(element.id) - 1;
-  const previousBodyPart = document.getElementById(`${previousBodyPartId}`);
-  console.log("EVALUATING NEW BODY PART");
-  console.log("previous body part:", previousBodyPart.className);
-  const previousBodyPartX = parseInt(
-    REGEX_TRANSLATE_FIRST_VALUE.exec(previousBodyPart.style.translate)[1]
-  );
-  const previousBodyPartY = parseInt(
-    REGEX_TRANSLATE_SECOND_VALUE.exec(previousBodyPart.style.translate)[1]
-  );
-  const currentBodyPartX = parseInt(REGEX_TRANSLATE_FIRST_VALUE.exec(element.style.translate)[1]);
-  const currentBodyPartY = parseInt(REGEX_TRANSLATE_SECOND_VALUE.exec(element.style.translate)[1]);
-
-  console.log(previousBodyPartX, previousBodyPartY, currentBodyPartX, currentBodyPartY);
-
-  if (previousBodyPartX > currentBodyPartX) {
-    console.log(previousBodyPartX, ">", currentBodyPartX, "returning right");
-    return "right";
-  } else if (previousBodyPartX < currentBodyPartX) {
-    console.log(previousBodyPartX, "<", currentBodyPartX, "returning left");
-    return "left";
-  } else if (previousBodyPartY > currentBodyPartY) {
-    console.log(previousBodyPartY, ">", currentBodyPartY, "returning down");
-    return "down";
-  } else if (previousBodyPartY < currentBodyPartY) {
-    console.log(previousBodyPartY, "<", currentBodyPartY, "returning up");
-    return "up";
-  }
-}
-
 function move(direction, element) {
-  const xValue = REGEX_TRANSLATE_FIRST_VALUE.exec(element.style.translate)[1];
-  const yValue = REGEX_TRANSLATE_SECOND_VALUE.exec(element.style.translate)[1];
+    switch (direction) {
+        case "up":
+            if (element.dataset.row == 0) return;
+            element.dataset.row = parseInt(element.dataset.row) - 1;
+            break;
+        case "down":
+            if (element.dataset.row == heightRatio - 1) return;
+            element.dataset.row = parseInt(element.dataset.row) + 1;
+            break;
+        case "left":
+            if (element.dataset.col == 0) return;
+            element.dataset.col = parseInt(element.dataset.col) - 1;
+            break;
+        case "right":
+            if (element.dataset.col == widthRatio - 1) return;
+            element.dataset.col = parseInt(element.dataset.col) + 1;
+            break;
+    }
 
-  let newXValue = xValue,
-    newYValue = yValue;
-  switch (direction) {
-    case "up":
-      newYValue = parseInt(yValue) - 100;
-      break;
-    case "down":
-      newYValue = parseInt(yValue) + 100;
-      break;
-    case "right":
-      newXValue = parseInt(xValue) + 100;
-      break;
-    case "left":
-      newXValue = parseInt(xValue) - 100;
-      break;
-  }
-
-  if (
-    newYValue < 0 ||
-    newXValue < 0 ||
-    newXValue > (widthRatio - 1) * 100 ||
-    newYValue > (heightRatio - 1) * 100
-  )
-    return;
-
-  element.style.translate = `${newXValue}% ${newYValue}%`;
+    element.style.translate = `${parseInt(element.dataset.col) * 100}% ${
+        parseInt(element.dataset.row) * 100
+    }%`;
 }
+
+function targetIsSnakeBody(col, row) {
+    const targetSpace = document.querySelector(`[data-col="${col}"][data-row="${row}"]`);
+    if (targetSpace !== null && targetSpace.className === "snake-body") {
+        return true;
+    } else {
+        return false;
+    }
+}
+function targetIsFood(col, row) {
+    const targetSpace = document.querySelector(`[data-col="${col}"][data-row="${row}"]`);
+    if (targetSpace === null) return;
+    if (targetSpace.className === "food") {
+        return true;
+    } else {
+        return false;
+    }
+}
+function eatFood(col, row) {
+    const targetFood = document.querySelector(`[data-col="${col}"][data-row="${row}"]`);
+    targetFood.remove();
+    foodEaten = true;
+}
+function snakeGrow() {
+    const snakeBodies = document.querySelectorAll(".snake-body");
+    const snakeBodiesArr = [...snakeBodies];
+    const lastBodyPart = snakeBodiesArr[snakeBodiesArr.length - 1];
+
+    const newBodyPart = document.createElement("div");
+    newBodyPart.classList.add("snake-body");
+    newBodyPart.dataset.col = lastBodyPart.dataset.col;
+    newBodyPart.dataset.row = lastBodyPart.dataset.row;
+    newBodyPart.id = parseInt(lastBodyPart.id) + 1;
+    newBodyPart.style.translate = `${parseInt(newBodyPart.dataset.col) * 100}% ${
+        parseInt(newBodyPart.dataset.row) * 100
+    }%`;
+
+    const gameContainer = document.querySelector(".game-container");
+    gameContainer.append(newBodyPart);
+}
+
+function moveBodyPart(nodeList) {
+    [...nodeList].reverse().forEach(snakeBody => {
+        const nextBodyPart = document.getElementById(`${snakeBody.id - 1}`);
+        snakeBody.dataset.next = nextBodyPart.dataset.next;
+        move(snakeBody.dataset.next, snakeBody);
+    });
+}
+
+var foodEaten = false;
+function generateFood() {
+    const randCol = Math.floor(Math.random() * widthRatio);
+    const randRow = Math.floor(Math.random() * heightRatio);
+
+    const newFood = document.createElement("div");
+    newFood.classList.add("food");
+
+    newFood.dataset.col = randCol;
+    newFood.dataset.row = randRow;
+    newFood.style.translate = `${randCol * 100}% ${randRow * 100}%`;
+
+    const gameContainer = document.querySelector(".game-container");
+    gameContainer.append(newFood);
+
+    foodEaten = false;
+}
+
+generateFood();
